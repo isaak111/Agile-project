6580bab9e3592337211337e28439288a62e068ff
Korvin1337 <korvin1337@gmail.com>
2021-12-21 13:30:31 +0000

    fixed animation for li

--- scripts/app.js
+++ scripts/app.js
@@ -14,22 +14,11 @@ staticList.addEventListener('mouseleave', function() {
     document.querySelector('#hover-list').style.display = "none";
     staticList.style.display = "flex";
 })*/
-
-let li = document.getElementsByClassName("li-class");
-
-for (let i = 0; i < li.length; i++) {
-  li[i].classList.add("pause");
-}
-console.log(li);
-
 (function () {
-  console.log("yolo");
-})();
+
+
 setTimeout(function(){
     document.body.className="";
-    let li = document.getElementsByClassName("li-class");
-    console.log(li);
-    for(let i = 0; i < li.length; i++) {
-      li[i].classList.remove("pause");
-    }
-},500);
+},1000);
+
+})();
